135be4f5608f56688140e914ccecc4c4a587fd60
Laur <sanduandreilaurentiu@gmail.com>
2024-12-07 20:03:19 +0200

    Modified Step4 and added edit button functionality

--- src/components/Step3.js
+++ src/components/Step3.js
@@ -1,26 +1,47 @@
-const Step3 = ({ formData, handleSubmit }) => (
-    <div>
-      <h2 className="mb-3">Step 3: Review Your Details</h2>
-      <ul className="list-group">
-        <li className="list-group-item">
-          <strong>Name:</strong> {formData.name}
-        </li>
-        <li className="list-group-item">
-          <strong>Email:</strong> {formData.email}
-        </li>
-        <li className="list-group-item">
-          <strong>Address:</strong> {formData.address}
-        </li>
-        <li className="list-group-item">
-          <strong>City:</strong> {formData.city}
-        </li>
-        <li className="list-group-item">
-          <strong>ZIP Code:</strong> {formData.zip}
-        </li>
-      </ul>
-     
+const Step3 = ({ formData, handleChange, errors }) => (
+  <div className="container mt-4">
+    <h2 className="mb-3">Step 3: Multi Information</h2>
+
+    
+    <div className="form-group mb-3">
+      <label className="form-label">Date of Birth</label>
+      <input
+        type="date"
+        className={`form-control ${errors.dob ? 'is-invalid' : ''}`}
+        name="dob"
+        value={formData.dob}
+        onChange={handleChange}
+      />
+      {errors.dob && <div className="invalid-feedback">{errors.dob}</div>}
     </div>
-  );
-  
-  export default Step3;
-  
+
+    
+    <div className="form-group mb-3">
+      <label className="form-label">Choose File to Upload</label>
+      <input
+        type="file"
+        className={`form-control ${errors.docpicker ? 'is-invalid' : ''}`}
+        name="docpicker"
+        accept=".pdf"
+        onChange={handleChange}
+      />
+      {errors.docpicker && <div className="invalid-feedback">{errors.docpicker}</div>}
+    </div>
+
+    
+    <div className="form-group mb-3">
+      <label className="form-label">Comment</label>
+      <textarea
+        rows={8}
+        className={`form-control ${errors.textarea ? 'is-invalid' : ''}`}
+        placeholder="Write a comment..."
+        name="textarea" 
+        value={formData.textarea}
+        onChange={handleChange}
+      />
+      {errors.textarea && <div className="invalid-feedback">{errors.textarea}</div>}
+    </div>
+  </div>
+);
+
+export default Step3;
