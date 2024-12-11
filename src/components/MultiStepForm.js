cd016ef484d8217ae49b42830237d005f40be367
Alfredo Poesina <alfredo@99codelines.com>
2024-12-11 20:18:44 +0200

    small change

--- src/components/MultiStepForm.js
+++ src/components/MultiStepForm.js
@@ -3,6 +3,7 @@ import Step1 from './Step1';
 import Step2 from './Step2';
 import Step3 from './Step3';
 import Step4 from './Step4';
+import Step5 from './Step5';
 
 
 const MultiStepForm = () => {
@@ -91,15 +92,13 @@ const MultiStepForm = () => {
       if (!formData.role.trim()) newErrors.role = 'Role is required';
     }
       
-    if (currentStep === 3) {
+    if (currentStep === 4) {
       if (!formData.docpicker.trim()) newErrors.docpicker = 'Pdf document is requierd'
       if (!formData.dob.trim()) newErrors.dob = 'Date of birth is requierd' }
       else if  (/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/.test(formData.dob)) {
         newErrors.dob = 'Date of birth is requierd';
         
       }
-      
-    
 
     return newErrors;
   };
@@ -110,8 +109,8 @@ const MultiStepForm = () => {
         {step === 1 && <Step1 formData={formData} handleChange={handleChange} errors={errors} />}
         {step === 2 && <Step2 formData={formData} handleChange={handleChange} errors={errors} />}
         {step === 3 && <Step3 formData={formData} handleChange={handleChange} errors={errors} />}
-        {step === 4 && <Step4 formData={formData} handleSubmit={handleSubmit} errors={errors} />}
-      
+        {step === 4 && <Step4 formData={formData} handleChange={handleChange} errors={errors} />}
+        {step === 5 && <Step5 formData={formData} handleSubmit={handleSubmit} errors={errors} />}
        
         <div className="d-flex justify-content-between mt-3">
           {step > 1 && (
@@ -119,7 +118,7 @@ const MultiStepForm = () => {
               Back
             </button>
           )}
-          {step < 4 ? (
+          {step < 5 ? (
             <button className="btn btn-primary" onClick={handleNext}>
               Next
             </button>
